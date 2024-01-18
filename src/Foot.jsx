d8c180eac0b63612cfef0514d317a7055c77e59c
Aro Barath Chandru B <chandru2021007@gmail.com>
2024-01-18 17:40:36 +0530

    first commit

---
+++ src/Foot.jsx
@@ -0,0 +1,15 @@
+function Foot() {
+    const food1 = "orange";
+    const food2 = "banana"
+    return(
+        <ul>
+            <li>Apple</li>
+            <li>{food1}</li>
+            <li>{food2}</li>
+            <li>{food1.toUpperCase()}</li>
+        </ul>
+    );
+
+}
+
+export default Foot;
